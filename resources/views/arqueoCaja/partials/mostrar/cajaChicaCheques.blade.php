4ec178434229442868603963b0bfc2e4e5aa7ad7
Enmanuel Quintero <qenmanuel2@gmail.com>
2024-11-13 21:00:54 -0600

    correcion tabla cheque

--- resources/views/arqueoCaja/partials/mostrar/cajaChicaCheques.blade.php
+++ resources/views/arqueoCaja/partials/mostrar/cajaChicaCheques.blade.php
@@ -6,7 +6,7 @@
 <div class="grid grid-cols-3 w-full border border-gray-300 rounded-lg overflow-hidden">
     <!-- Encabezado de la tabla -->
     <div class="bg-gray-200 p-2 font-semibold text-center border border-gray-300">Numero de Cheque</div>
-    <div class="bg-gray-200 p-2 font-semibold text-center border border-gray-300">Banco</div>
+    <div class="bg-gray-200 p-2 font-semibold text-center border border-gray-300">Nombre y Apellido</div>
     <div class="bg-gray-200 p-2 font-semibold text-center border border-gray-300">Monto</div>
 
     <!-- Filas de la tabla -->
